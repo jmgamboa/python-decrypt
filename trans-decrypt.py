cf9c31d6679bdc1fbbc24616d7a681effa4b3b75
Joaquin Gamboa <gamboanj@gmail.com>
2015-08-13 11:22:40 -0400

    first commit

---
+++ trans-decrypt.py
@@ -0,0 +1,9 @@
+
+from string import maketrans   # Required to call maketrans function.
+
+intab = "mqg"
+outtab = "koeabcsdflskalskdjficlslweioupzxcvn"
+trantab = maketrans(intab, outtab)
+
+str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.";
+print str.translate(trantab)
